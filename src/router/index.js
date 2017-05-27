fad1afdff7dc74213e0131d5eecf2ea51e24f1ac
gongyanggao <1466563549@qq.com>
2017-05-27 16:08:47 +0800

    5-27-1

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 // UI Element Groups
-import Login from '../pages/login'
-import Home from '../pages/Home.vue'
-import ContentWrap from '../pages/ContentWrap.vue'
-import OverView from '../pages/overview'
-import ViewDetail from '../pages/viewdetail'
-import ChartPage from '../pages/chartpage'
+import routes from '../component/'
+import example from './example'
+import form from './forms'
 
 // forms
 Vue.use(Router)
@@ -16,35 +13,16 @@ export default new Router({
   mode: 'history',
   routes: [{
     path: '/',
+    hidden: true,
     redirect: '/login'
   }, {
     path: '/login',
     name: '登录',
-    component: Login
-  }, {
-    path: '/home',
-    name: '首页',
-    redirect: '/home/content',
-    component: Home,
-    children: [{
-      path: 'content',
-      name: '反诈',
-      component: ContentWrap,
-      redirect: '/home/content/overview',
-      children: [{
-        path: 'overview',
-        name: '总体信息',
-        component: OverView
-      }, {
-        path: 'viewdetail/:id',
-        name: '诈骗详情页',
-        component: ViewDetail
-      }, {
-        path: 'chartpage',
-        name: '图表展示',
-        component: ChartPage
-      }]
-    }]
-  }],
+    hidden: true,
+    component: routes.Login
+  }]
+  .concat(example)
+  .concat(form)
+  ,
   linkActiveClass: 'active'
 })
